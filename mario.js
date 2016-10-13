688937df8807823fb3ec1d54f12f5fc68fc8e91d
The Shizz <The Shizz>
2016-10-13 15:37:45 -0500

    created dynamic slider to draw pyramid on-the-fly s. Lines starting

---
+++ mario.js
@@ -0,0 +1,93 @@
+$(function() {
+    var $document   = $(document),
+        $inputRange = $('input[type="range"]');
+    
+    // Example functionality to demonstrate a value feedback
+    function valueOutput(element) {
+        var value = element.value,
+            output = element.parentNode.getElementsByTagName('output')[0];
+        output.innerHTML = value;
+    }
+    for (var i = $inputRange.length - 1; i >= 0; i--) {
+        valueOutput($inputRange[i]);
+    };
+    $document.on('input', 'input[type="range"]', function(e) {
+        valueOutput(e.target);
+    });
+    // end
+  
+    $inputRange.rangeslider({
+      polyfill: false 
+    });
+});
+
+/**
+ * determineHeightAndThenDrawPyramid
+ *
+ * Determines the current value that the user has typed in the 'How high?' text-box,
+ * and then draws a pyramid with that height.
+ */
+function determineHeightAndThenDrawPyramid() {
+
+    // just so we know we're here
+    console.log("someone invoked the determineHeightAndThenDrawPyramid function!");
+
+    // TODO 3
+    // figure out the height the user typed (replace the "5" below)
+    heightStr = document.getElementById("height").value;
+
+    // here we convert the string to an int
+    height = parseInt(heightStr);
+
+    // TODO 2
+    // draw the pyramid with the given height
+	
+	drawPyramid(height);
+
+}
+
+
+// TODO 1
+// hook up the button's click event to our determineHeightAndThenDrawPyramid function
+
+
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
+ function drawPyramid(height) {
+
+     // TODO 4
+     // before drawing, clear the old content
+
+	document.getElementById("pyramid").innerHTML = height;
+
+     // for each row....
+     for (var row = 0; row < height; row++) {
+
+         // figure out number of bricks and spaces
+         var numBricks = row + 2;
+         var numSpaces = height - row - 1;
+
+         // build up a string for this row
+         var rowStr = "";
+         for (var i = 0; i < numSpaces; i++) {
+             rowStr += ".";
+         }
+         for (var i = 0; i < numBricks; i++) {
+             rowStr += "#";
+         }
+
+        // create a text element with the string of characters
+        textElem = document.createTextNode(rowStr);
+
+        // create a <p> element with the text inside
+        rowElem = document.createElement("p");
+        rowElem.appendChild(textElem);
+
+        // insert the paragraph as a child of the container <div>
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
+}
